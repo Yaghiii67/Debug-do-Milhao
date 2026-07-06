f522a34f82bd0c3099f1e9950d494f2e6685fcbd
Tedwarf <henrique.sdourado@hotmail.com>
2026-07-06 13:13:48 -0300

    Atulizado

--- src/View/TelaBase.java
+++ src/View/TelaBase.java
@@ -1,4 +1,4 @@
-package View;
+package ViewVisual;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,6 +15,16 @@ public abstract class TelaBase extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
+
+        ImageIcon imagem = new ImageIcon(
+                getClass().getResource("/imagens/FUNDO.png"));
+
+        JLabel fundo = new JLabel(imagem);
+
+        fundo.setLayout(new BorderLayout());
+
+        setContentPane(fundo);
+
 
         montarConteudo();
     }
