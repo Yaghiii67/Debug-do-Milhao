87a6d9c5fb1ccd0415b339459f1ec406e4fa8d2c
Tedwarf <henrique.sdourado@hotmail.com>
2026-07-06 14:13:25 -0300

    Atualização Final

--- src/View/TelaBase.java
+++ src/View/TelaBase.java
@@ -1,28 +1,33 @@
 package View;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Font;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.*;
 
 public abstract class TelaBase extends JFrame {
-    protected static final Font FONTE_TITULO = new Font("SansSerif", 1, 26);
-    protected static final Font FONTE_TEXTO = new Font("SansSerif", 0, 16);
-    protected static final Font FONTE_BOTAO = new Font("SansSerif", 0, 16);
+
+    protected static final Font FONTE_TITULO = new Font("SansSerif", Font.BOLD, 26);
+    protected static final Font FONTE_TEXTO = new Font("SansSerif", Font.PLAIN, 16);
+    protected static final Font FONTE_BOTAO = new Font("SansSerif", Font.PLAIN, 16);
 
     public TelaBase(String titulo) {
         super(titulo);
-        this.setSize(600, 450);
-        this.setDefaultCloseOperation(2);
-        this.setLocationRelativeTo((Component)null);
-        this.setResizable(false);
-        ImageIcon imagem = new ImageIcon(this.getClass().getResource("/imagens/FUNDO.png"));
+        setSize(854, 480);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        ImageIcon imagem = new ImageIcon(
+                getClass().getResource("/imagens/FUNDO.png"));
+
         JLabel fundo = new JLabel(imagem);
+
         fundo.setLayout(new BorderLayout());
-        this.setContentPane(fundo);
-        this.montarConteudo();
+
+        setContentPane(fundo);
+
+
+
+        montarConteudo();
     }
 
     protected abstract void montarConteudo();
